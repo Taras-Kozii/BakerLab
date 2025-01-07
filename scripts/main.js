dbb5be234eaa2ddcf7384a52d3c7aeae41fff72f
Taras Kozii <103899071+Taras-Kozii@users.noreply.github.com>
2025-01-07 15:41:44 +0000

    version v1

--- scripts/main.js
+++ scripts/main.js
@@ -3,9 +3,45 @@
 const page = document.querySelector('.page');
 const menu = document.querySelector('.header__menu');
 const burger = document.querySelector('.burger');
+const navLinks = document.querySelectorAll('[data-goto]');
+const header = document.querySelector('.header');
 
 burger.addEventListener('click', () => {
   burger.classList.toggle('active');
   menu.classList.toggle('active');
-  page.classList.toggle('lock');
+  lockPage();
 });
+[...navLinks].forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    burger.classList.remove('active');
+    menu.classList.remove('active');
+
+    if (page.classList.contains('lock')) {
+      lockPage();
+    }
+    window.scrollTo({
+      top: getScrollToBlockValue(e.target),
+      behavior: "smooth",
+    });
+  })
+})
+
+
+function lockPage() {
+  const paddingRight = window.innerWidth - document.documentElement.clientWidth + 'px';
+  setFixPadding(paddingRight);
+  page.classList.toggle('lock');
+}
+function setFixPadding(paddingRight) {
+  const fixItems = document.querySelectorAll('.right-fix-padding');
+  [...fixItems].forEach(item => {
+    item.style.paddingRight = paddingRight;
+  });
+}
+function getScrollToBlockValue(link) {
+  const blockName = link.dataset.goto;
+  const block = document.querySelector(blockName);
+  const value = block.getBoundingClientRect().top + window.scrollY - header.offsetHeight;
+  return value || 0;
+}
